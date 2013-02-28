a0c167716e027a23cff308020f29ddf4a803c1e4
Daniel Broudy <daniel.broudy@gmail.com>
2013-02-27 21:32:34 -0500

    work on part 3

--- hw2/neural_net_impl.py
+++ hw2/neural_net_impl.py
@@ -40,9 +40,9 @@ def FeedForward(network, input):
   """
   network.CheckComplete()
   # 1) Assign input values to input nodes
-  for i in range(len(input.values)):
+  for i in range(len(network.inputs)):
       network.inputs[i].raw_value = input[i]
-      network.inputs[i].transformed_value = NeuralNetwork.Sigmoid(network.inputs[i].raw_value)) 
+      network.inputs[i].transformed_value = NeuralNetwork.Sigmoid(network.inputs[i].raw_value) 
 
   # 2) Propagates to hidden layer
   for node in network.hidden_nodes:
@@ -103,29 +103,33 @@ def Backprop(network, input, target, learning_rate):
   """
   network.CheckComplete()
   # 1) We first propagate the input through the network
-  # CONSIDER: should this be NetworkFramework.FeedForward? or something like that.
   FeedForward(network,input)
 
-  # assume that a
   # 2) Then we compute the errors and update the weigths starting with the last layer
   for i in range(len(network.outputs)):
       node = network.outputs[i]
       err = target[i] - node.transformed_value
+      node.delta = err * NeuralNetwork.SigmoidPrime(node.transformed_value) 
       for j in range(len(node.weights)):
           # update node weight for each hidden_node.
-          # lecture6-notes page 7     w_mj <- w_mj + alpha * a_j * delta_j
+          # lecture6-notes page 7     w_mj <- w_mj + alpha * a_m * delta_j
           # I THINK SOMETHING IS WRONG WITH THIS.
-          node.delta = err * NeuralNetwork.SigmoidPrime(node.transformed_value) 
-          node.weights[j] = node.weights[j] + (learning_rate * node.inputs[j].transformed_value * delta
+          # do we need to save the old weight? node.old_weights[j] = copy.deep(node.weights[j])
+          node.weights[j] = node.weights[j] + (learning_rate * node.inputs[j].transformed_value * node.delta)
 
   # 3) We now propagate the errors to the hidden layer, and update the weights there too
   num = len(network.hidden_nodes)
   #this will loop backwards from last node in hidden nodes
   # range(from, to, increment) eg.  range (9,-1,-1) = [ 9,8,7,6,5,4,3,2,1,0 ] because zero indexed we to go -1
-  for i in range(num-1,-1,-1)):
+  for i in range(num-1,-1,-1):
+#looping through hidden nodes:
       node = network.hidden_nodes[i]
-      #fore each hidden node, update the weights.
-  
+      error = 0
+      for j in range(len(node.forward_neighbors)):
+          error = error + node.forward_neighbors[j].weight * node.forward_neighbors[j].delta
+      node.delta = NeuralNetwork.SigmoidPrime(node.transformed_value) * error
+      for k in range(len(node.inputs)):
+          node.weight[k] = node.weight[k] + (learning_rate * node.inputs[k].transformed_value * node.delta)
 
 # <--- Problem 3, Question 3 --->
 
@@ -149,9 +153,10 @@ def Train(network, inputs, targets, learning_rate, epochs):
   run the *Backprop* over the training set *epochs*-times
   """
   network.CheckComplete()
-  pass
-  
-
+#def Backprop(network, input, target, learning_rate):
+  for i in range(epochs):
+      for j in range(len(inputs)):
+          Backprop(network, inputs[j], targets[j], learning_rate)      
 
 # <--- Problem 3, Question 4 --->
 
@@ -254,7 +259,7 @@ class EncodedNetworkFramework(NetworkFramework):
             new_sublst.append(sublst[i]/256.0)
         return new_sublst
 
-    return map(norm_sublst, pixels)
+    return map(norm_sublst, image.pixels)
 
   def InitializeWeights(self):
     """
