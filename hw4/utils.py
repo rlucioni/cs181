c333df74622afe4f7606258858a76cccb7dbf7d0
Daniel Broudy <daniel.broudy@gmail.com>
2013-04-07 17:46:55 -0400

    some work on 2ab, most of 2 done starting to tex it up

--- hw4/utils.py
+++ hw4/utils.py
@@ -3,13 +3,12 @@ import math
 
 #Cumulative Distribution Function
 #P (X<=x) = 1/2 erfc((mu-x)/(sqrt(2) sigma))
-def cdf(mu, sigma,x):
-    return (lamda x: (1/2)* erfc((mu-x) / (sigma * sqrt(2))))
+def cdf(mu, sigma): return lambda x: (1/2)* math.erfc((mu-x) / (sigma * math.sqrt(2)))
 
 #Normal Distribution (Gaussian Distribution)
 #e^(-(x-mu)^2/(2 sigma^2))/(sqrt(2 pi) sigma)
-def norm(mu,var,x):
-    sigma = sqrt(var)
-    return (lamda x: exp((-(x-mu)^2)/(2*var))/(sqrt(2*pi)*sigma))
+def norm(mu,var):
+    sigma = math.sqrt(var)
+    return lambda x: exp((-(x-mu)^2)/(2*var))/(math.sqrt(2*pi)*sigma)
 
 
