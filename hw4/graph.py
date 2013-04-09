abd601d5bd128fbcf8fffe400fa8d6c46519ec31
Daniel Broudy <daniel.broudy@gmail.com>
2013-04-09 00:27:13 -0400

    finished 2

--- hw4/graph.py
+++ hw4/graph.py
@@ -8,22 +8,26 @@ plt.clf()
 
 # (0.2*NormalDistribution[1, 25]) + (0.3*NormalDistribution[-2, 1]) + (0.5*NormalDistribution[3, 4])
 factor1 = .2
-#factor1 = .02
 mean1 = 1
 variance1 = 2
 sigma1 = sqrt(variance1)
 
 factor2 = .3
-#factor2 = .03
 mean2 = -2
 variance2 = 1
 sigma2 = sqrt(variance2)
 
 factor3 = .5
-#factor3 = .05
 mean3 = 3
 variance3 = 4
 sigma3 = sqrt(variance3)
+
+
+factorwrap = 1
+meanwrap = 3
+variancewrap = 4
+sigmawrap = sqrt(variance3)
+
 
 #x = arange(-10,10,1)
 #x = arange(-7,10,.01)
@@ -33,6 +37,7 @@ func1 =factor1* mlab.normpdf(x,mean1,sigma1)
 func2 =factor2* mlab.normpdf(x,mean2,sigma2)
 func3 =factor3* mlab.normpdf(x,mean3,sigma3)
 
+funcwrap =factorwrap* mlab.normpdf(x,meanwrap,sigmawrap)
 #cdf1 = utils.cdf(mean1, sigma1)
 #y = map(cdf1, x)
 #plot(x,y)
@@ -40,9 +45,12 @@ func3 =factor3* mlab.normpdf(x,mean3,sigma3)
 #plt.plot(x,cdf1(x))
 
 plt.plot(x,func1+func2+func3)
+
+plt.plot(x,funcwrap)
 #plt.plot(x,mlab.normpdf(x,mean1,sigma1))
 #plt.plot(x,intg1)
 
 plt.show
 #savefig('test.pdf')
-savefig('mixture_o_gaussians.pdf')
+#savefig('mixture_o_gaussians.pdf')
+savefig('mixture_o_gaussians_thats_a_wrap.pdf')
