b01f3262aaacc2ba03f464b50026c490b2a064a7
Daniel Broudy <daniel.broudy@gmail.com>
2013-04-08 21:52:32 -0400

    changes to utils and tex file

--- hw4/graph.py
+++ hw4/graph.py
@@ -1,6 +1,6 @@
-#import matplotlib.pyplot as plt
-#import numpy as np
-#import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.mlab as mlab
 import utils
 from pylab import *
 
@@ -20,23 +20,23 @@ mean3 = 3
 variance3 = 4
 sigma3 = sqrt(variance3)
 
-x = arange(-7,10,.01)
-#x=  np.linspace(-7,10,100)
+#x = arange(-7,10,.01)
+x=  np.linspace(-7,10,100)
 
-#func1 =factor1* mlab.normpdf(x,mean1,sigma1)
-#func2 =factor2* mlab.normpdf(x,mean2,sigma2)
-#func3 =factor3* mlab.normpdf(x,mean3,sigma3)
+func1 =factor1* mlab.normpdf(x,mean1,sigma1)
+func2 =factor2* mlab.normpdf(x,mean2,sigma2)
+func3 =factor3* mlab.normpdf(x,mean3,sigma3)
 
-cdf1 = utils.cdf(mean1, sigma1)
-y = map(cdf1, x)
-plot(x,y)
-show()
+#cdf1 = utils.cdf(mean1, sigma1)
+#y = map(cdf1, x)
+#plot(x,y)
+#show()
 #plt.plot(x,cdf1(x))
 
-#plt.plot(x,func1+func2+func3)
+plt.plot(x,func1+func2+func3)
 #plt.plot(x,mlab.normpdf(x,mean1,sigma1))
 #plt.plot(x,intg1)
 
-#plt.show
+plt.show
 #savefig('test.pdf')
-#savefig('mixture_o_gaussians.pdf')
+savefig('mixture_o_gaussians.pdf')
--- hw4/utils.py
+++ hw4/utils.py
@@ -1,14 +1,17 @@
-import math
+from math import *
+import scipy.special as sp
 
 
 #Cumulative Distribution Function
 #P (X<=x) = 1/2 erfc((mu-x)/(sqrt(2) sigma))
-def cdf(mu, sigma): return lambda x: (1/2)* math.erfc((mu-x) / (sigma * math.sqrt(2)))
+def cdf(mu, sigma): return lambda x: (1/2)* erfc((mu-x) / (sigma * sqrt(2)))
+
+def cdfinv(mu, sigma): return lambda y: (mu - (sqrt(2)*sigma * sp.erfcinv(2*y)))
 
 #Normal Distribution (Gaussian Distribution)
 #e^(-(x-mu)^2/(2 sigma^2))/(sqrt(2 pi) sigma)
 def norm(mu,var):
     sigma = math.sqrt(var)
-    return lambda x: exp((-(x-mu)^2)/(2*var))/(math.sqrt(2*pi)*sigma)
+    return lambda x: exp((-(x-mu)^2)/(2*var))/(sqrt(2*pi)*sigma)
 
 
