be2bab1325a06519d706db08f6d7de92efb51d1b
Renzo Lucioni <renzolucioni@gmail.com>
2013-04-08 21:39:41 -0400

    working on 3

--- hw4/graph.py
+++ hw4/graph.py
@@ -7,40 +7,40 @@ from pylab import *
 plt.clf()
 
 # (0.2*NormalDistribution[1, 25]) + (0.3*NormalDistribution[-2, 1]) + (0.5*NormalDistribution[3, 4])
-#factor1 = .2
-factor1 = .02
+factor1 = .2
+#factor1 = .02
 mean1 = 1
 variance1 = 2
 sigma1 = sqrt(variance1)
 
-#factor2 = .3
-factor2 = .03
+factor2 = .3
+#factor2 = .03
 mean2 = -2
 variance2 = 1
 sigma2 = sqrt(variance2)
 
-#factor3 = .5
-factor3 = .05
+factor3 = .5
+#factor3 = .05
 mean3 = 3
 variance3 = 4
 sigma3 = sqrt(variance3)
 
 #x = arange(-10,10,1)
-x = arange(-7,10,.01)
-x=  np.linspace(-7,10,100)
+#x = arange(-7,10,.01)
+x = np.linspace(-7,10,100)
 
 func1 =factor1* mlab.normpdf(x,mean1,sigma1)
 func2 =factor2* mlab.normpdf(x,mean2,sigma2)
 func3 =factor3* mlab.normpdf(x,mean3,sigma3)
 
-cdf1 = utils.cdf(mean1, sigma1)
-y = map(cdf1, x)
-plot(x,y)
+#cdf1 = utils.cdf(mean1, sigma1)
+#y = map(cdf1, x)
+#plot(x,y)
 #show()
 #plt.plot(x,cdf1(x))
 
 plt.plot(x,func1+func2+func3)
-plt.plot(x,mlab.normpdf(x,mean1,sigma1))
+#plt.plot(x,mlab.normpdf(x,mean1,sigma1))
 #plt.plot(x,intg1)
 
 #plt.show
