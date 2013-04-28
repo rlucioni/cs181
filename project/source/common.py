0c7b592ecafe710ab194a9f59bf51e6d88cfca83
Renzo Lucioni <renzolucioni@gmail.com>
2013-04-28 01:52:21 -0400

    WE GOT IMAGES

--- project/source/common.py
+++ project/source/common.py
@@ -2,19 +2,39 @@ import game_interface
 import random
 import time
 
-def get_move(view):
+#def get_move(view):
+def get_move(view,img_filename,val_filename):
   # Choose a random direction.
   # If there is a plant in this location, then try and eat it.
   hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
   # Choose a random direction
-  f = open("images.txt", "a+")
-  f.write(str(view.GetLife())+'\n')
+  f = open(val_filename, "r+")
+  # assumes we only have numbers as strings
+  prev_val = int(f.readline())
+  
+  cur_val = view.GetLife()
+  f.seek(0)
+  f.write(str(cur_val)+'\n')
+  f.flush()
+  f.close()
+  
+  g = open(img_filename, "a+")
+  diff = cur_val - prev_val
+  # label the previous images
+  if diff > 10:
+    g.write('NUTRITIOUS\n######\n')
+  elif diff < -10:
+    g.write('POISONOUS\n######\n')
+  g.flush()
+  g.close()
+ 
   if hasPlant:
+    g = open(img_filename, "a+")
     for i in xrange(5):
       #print view.GetImage()
-      f = open("images.txt", "a+")
-      f.write(str(view.GetImage())+'\n')
-  f.close()
+      g.write(str(view.GetImage())+'\n')
+    g.flush()
+    g.close()
 
   time.sleep(0.1)
   return (random.randint(0, 4), hasPlant)
--- project/source/player1/player.py
+++ project/source/player1/player.py
@@ -1,4 +1,5 @@
 import common
 
 def get_move(view):
-  return common.get_move(view)
+  #return common.get_move(view)
+  return common.get_move(view,"images1.txt","value1.txt")
