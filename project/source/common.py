63add34626d2dfc10c583b75b9b15f4f5eac78cb
Renzo Lucioni <renzolucioni@gmail.com>
2013-04-28 00:37:56 -0400

    trying to extract and clasify images

--- project/source/common.py
+++ project/source/common.py
@@ -7,8 +7,14 @@ def get_move(view):
   # If there is a plant in this location, then try and eat it.
   hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
   # Choose a random direction
+  f = open("images.txt", "a+")
+  f.write(str(view.GetLife())+'\n')
   if hasPlant:
     for i in xrange(5):
-      print view.GetImage()
+      #print view.GetImage()
+      f = open("images.txt", "a+")
+      f.write(str(view.GetImage())+'\n')
+  f.close()
+
   time.sleep(0.1)
   return (random.randint(0, 4), hasPlant)
