635b5487c5a3edaad2b4dc91439f7c7a7f93ef8d
Renzo Lucioni <renzolucioni@gmail.com>
2013-05-02 15:22:01 -0400

    q-learning in player

--- project/source/player1/__init__.py
+++ project/source/player1/__init__.py
@@ -1,3 +1,5 @@
 import neural_net_main
+import modelfree
 
 network = neural_net_main.reconstruction()
+q_table = modelfree.Load_Q_table()
--- project/source/player1/player.py
+++ project/source/player1/player.py
@@ -2,15 +2,46 @@ import common
 import game_interface
 import random
 import time
+import copy
 from data_reader import *
 from __init__ import *
+from modelfree import *
+
+ATE_NUTRITIOUS = 0
+ATE_POISONOUS = 1
+SEEN_NOTHING = 2
+PASSED = 3
 
 #def get_move(view):
 #  #return common.get_move(view)
 #  return common.get_move(view,"images1.txt","value1.txt")
 
 def get_move(view):
-  hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT 
+  hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
+
+  cur_life = view.GetLife()
+  
+  if hasattr(view, 'previous_life'):
+    diff = cur_life - view.previous_life
+    view.previous_life = cur_life
+  else:
+    diff = 0
+
+  if hasattr(view, 'old_hasPlant'):
+    if view.old_hasPlant == 0:
+      cur_state = SEEN_NOTHING
+    elif view.ate == 0:
+      cur_state = PASSED
+    elif diff < game_interface.plant_penalty:
+      cur_state = ATE_POISONOUS
+    elif diff > 1:
+      cur_state = ATE_NUTRITIOUS
+
+  view.old_hasPlant = hasPlant
+
+  if hasattr(view, 'prev_state'):
+    Q_learn_it(q_table, view.prev_state, view.prev_action, cur_state, diff)
+
   eat = -1
   if hasPlant:
     nutritious_count = 0
@@ -22,11 +53,15 @@ def get_move(view):
       else:
         nutritious_count -= 1
 
-      if nutritious_count == 1:
+      if nutritious_count == 2:
         eat = 1
-      elif nutritious_count == -1:
+      elif nutritious_count == -2:
         eat = 0
 
+  view.ate = eat
+  view.prev_state = cur_state
+  view.prev_action = Q_get_move(q_table)
+
   time.sleep(0.1)
-  return (random.randint(0, 4), eat)
+  return (view.prev_action, eat)
   #return (1, eat)
