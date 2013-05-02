8a2c0edb0f3027c5858b00aa31b0f50ab22b054d
Daniel Broudy <daniel.broudy@gmail.com>
2013-05-02 15:24:35 -0400

    fixed game penalties

--- project/source/run_game.py
+++ project/source/run_game.py
@@ -34,16 +34,16 @@ def get_move(view, cmd, options, player_id):
   return (mv, eat)
 
 def run(options):
- # game = game_interface.GameInterface(options.plant_bonus,
-  #                                    options.plant_penalty,
-   #                                   options.observation_cost,
-    #                                  options.starting_life,
-     #                                 options.life_per_turn)
   game = game_interface.GameInterface(options.plant_bonus,
-                                      0,#options.plant_penalty,
-                                      0,#options.observation_cost,
-                                      10,#options.starting_life,
-                                      0)#options.life_per_turn)
+                                      options.plant_penalty,
+                                      options.observation_cost,
+                                      options.starting_life,
+                                      options.life_per_turn)
+#  game = game_interface.GameInterface(options.plant_bonus,
+ #                                     0,#options.plant_penalty,
+  #                                    0,#options.observation_cost,
+   #                                   10,#options.starting_life,
+    #                                  0)#options.life_per_turn)
   player1_view = game.GetPlayer1View()
   player1_view = game.GetPlayer1View()
   player2_view = game.GetPlayer2View()
